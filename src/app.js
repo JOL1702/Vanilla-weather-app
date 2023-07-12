ad4bc56b7d51ed64f32a1d226d3ea0601e1c8aba
JOL1702 <138676560+JOL1702@users.noreply.github.com>
2023-07-12 20:00:05 +0100

    Updated content of JS src

--- src/app.js
+++ src/app.js
@@ -1,1 +1,72 @@
+let now = new Date();
 
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let weekday = weekdays[now.getDay()];
+
+let currentDay = `${weekday} ${hours}:${minutes}`;
+
+let todaysDate = document.querySelector("#todaysDate");
+todaysDate.innerHTML = `${currentDay}`;
+
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#weatherDescription").innerHTML =
+    response.data.weather[0].main;
+}
+
+function searchCity(city) {
+  let apiKey = "0e8cef4f92bb23c422b20a45e433e172";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#locationForm-input").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "0e8cef4f92bb23c422b20a45e433e172";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+  console.log(apiUrl);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let locationForm = document.querySelector("#locationForm");
+locationForm.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#currentLocationButton");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("London");
